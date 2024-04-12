02d0668e3f6219180e378519dc279bf3f9dd5ddb
mjkingtech <mjkingtech@gmail.com>
2024-04-12 16:37:11 +0530

    add address

--- src/controllers/adminOrder.controller.js
+++ src/controllers/adminOrder.controller.js
@@ -1,4 +1,5 @@
 
+const Address = require("../models/address.model");
 const orderService = require("../services/order.service");
 
 const getAllOrders = async (req, res) => {
@@ -9,7 +10,14 @@ const getAllOrders = async (req, res) => {
     res.status(500).send({ error: "Something went wrong" });
   }
 };
-
+const getAllOrdersAddress = async (req, res) => {
+  try {
+    const address = await Address.find();
+    return res.status(202).send(address);
+  } catch (error) {
+    res.status(500).send({ error: "Something went wrong" });
+  }
+};
 const confirmedOrder = (req, res) => {
   try {
     const orderId = req.params.orderId;
@@ -71,4 +79,5 @@ module.exports = {
   deliverOrder,
   cancelledOrder,
   deleteOrder,
+  getAllOrdersAddress
 };
--- src/routes/adminOrder.routes.js
+++ src/routes/adminOrder.routes.js
@@ -4,6 +4,7 @@ const router=express.Router();
 const adminOrderController=require("../controllers/adminOrder.controller.js")
 
 router.get("/",authenticate,adminOrderController.getAllOrders);
+router.get("/alladdress",authenticate,adminOrderController.getAllOrdersAddress);
 router.put("/:orderId/confirmed",authenticate,adminOrderController.confirmedOrder);
 router.put("/:orderId/ship",authenticate,adminOrderController.shippOrder);
 router.put("/:orderId/deliver",authenticate,adminOrderController.deliverOrder);
