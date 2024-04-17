c7e5fe072e067f41100b6a31ab04f57abebb16a6
mjkingtech <mjkingtech@gmail.com>
2024-04-17 11:03:30 +0530

    modifydb

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 // const mongoDbUrl='mongodb+srv://codewithzosh:lognBCBmtWNPjrKC@cluster0.wvt9jpw.mongodb.net/?retryWrites=true&w=majority'
 const connectDb= async()=>{
      try {
-        const connectionInstance = await mongoose.connect(process.env.DB_URL);
+        const connectionInstance = await mongoose.connect(process.env.DB_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
         
      } catch (error) {
         console.log('datebase errror:', error);
