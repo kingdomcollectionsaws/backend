9a40203b62b27894ce718aa5104d6e0b57b48883
mjkingtech <mjkingtech@gmail.com>
2024-04-29 12:36:49 +0530

    dljdijf

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -2,7 +2,6 @@ const express=require("express");
 const authenticate = require("../middleware/authenticat.js");
 const router=express.Router();
 const paymentController=require("../controllers/payment.controller.js");
-//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
 router.post("/:id",authenticate,paymentController.createPaymentLink);
 router.get("/",authenticate,paymentController.updatePaymentInformation);
 
