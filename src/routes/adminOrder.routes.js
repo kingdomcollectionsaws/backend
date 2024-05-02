5f22ed470d8bdc2238326ff17d2245635a96ce82
mjkingtech <mjkingtech@gmail.com>
2024-05-02 14:28:46 +0530

    loaction

--- src/routes/adminOrder.routes.js
+++ src/routes/adminOrder.routes.js
@@ -3,7 +3,7 @@ const authenticate = require("../middleware/authenticat.js");
 const router=express.Router();
 const adminOrderController=require("../controllers/adminOrder.controller.js");
 const { createShipping } = require("../controllers/fedexauth.js");
-
+const getlocation = require("../services/location.js");
 router.get("/",authenticate,adminOrderController.getAllOrders);
 router.get("/alladdress",authenticate,adminOrderController.getAllOrdersAddress);
 router.get("/allusers", adminOrderController.getAllusers);
@@ -13,5 +13,6 @@ router.put("/:orderId/deliver",authenticate,adminOrderController.deliverOrder);
 router.put("/:orderId/cancel",authenticate,adminOrderController.cancelledOrder);
 router.delete("/:orderId/delete",authenticate,adminOrderController.deleteOrder);
 router.post("/fedex/shiping",authenticate,createShipping);
+router.get("/location",getlocation);
 
 module.exports=router;
---
+++ src/services/location.js
@@ -0,0 +1,17 @@
+const { default: axios } = require("axios")
+
+const getlocation = async(req,res)=>{
+    try {
+        const response = await fetch('http://www.geoplugin.net/json.gp');
+        // if (!response.ok) {
+        //   throw new Error('Failed to fetch data');
+        // }
+        const data = await response.json();
+        console.log(data);
+       return data
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+}
+getlocation()
+module.exports = getlocation;
