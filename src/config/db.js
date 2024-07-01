89cae722e553b6eb7c7fb8186d4b3948fcb7c838
kingdomcollectionsaws <kingdomcollectionsaws@gmail.com>
2024-07-01 21:41:44 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 // const mongoDbUrl='mongodb+srv://codewithzosh:lognBCBmtWNPjrKC@cluster0.wvt9jpw.mongodb.net/?retryWrites=true&w=majority'
 const connectDb= async()=>{
      try {
-        const connectionInstance = await mongoose.connect(process.env.DB_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
+        const connectionInstance = await mongoose.connect("mongodb+srv://kingdomcollection:kingdomcollection@cluster0.xsmqk48.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{ useNewUrlParser: true, useUnifiedTopology: true });
         
      } catch (error) {
         console.log('datebase errror:', error);
